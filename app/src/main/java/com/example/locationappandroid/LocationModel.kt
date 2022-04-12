0c4052a22dc2429affdaf74711ed0a491fc940a0
SachPrecious <sachithrabandara49@gmail.com>
2022-04-12 08:52:05 +0530

    Updated

--- app/src/main/java/com/example/locationappandroid/LocationModel.kt
+++ app/src/main/java/com/example/locationappandroid/LocationModel.kt
@@ -1,4 +1,24 @@
 package com.example.locationappandroid
 
-class LocationModel {
+
+import java.util.*
+
+data class LocationModel (
+    var id: Int = getAutoId(),
+    var loc:String = "",
+    var longitude:String = "",
+    var latitude:String = ""
+
+){
+
+
+    companion object
+    {
+        fun getAutoId():Int{
+            val random = Random()
+            return random.nextInt(100)
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/example/locationappandroid/MainActivity.kt
@@ -0,0 +1,153 @@
+package com.example.locationappandroid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var edlocation:EditText
+    private lateinit var edlongitude:EditText
+    private lateinit var edlatitude:EditText
+
+    private lateinit var btnAdd:Button
+    private lateinit var btnView:Button
+
+    private lateinit var sqLiteHelper: SQLiteHelper
+    private lateinit var recyclerView: RecyclerView
+    private var adapter : LocationAdapter? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initView()
+        initRecycleView()
+        sqLiteHelper= SQLiteHelper(this)
+        btnAdd.setOnClickListener { addLocation() }
+        btnView.setOnClickListener { getLocations() }
+
+        adapter?.setOnClickItem {
+
+            val intent = Intent(this, MapsFragment::class.java)
+            //Open Maps Fragment
+            intent.putExtra("name",it.loc)
+            intent.putExtra("lat",it.latitude)
+            intent.putExtra("lng",it.longitude)
+
+            startActivity(intent)
+
+            Toast.makeText(this, it.loc, Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, it.latitude, Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, it.longitude, Toast.LENGTH_SHORT).show()
+
+
+
+
+        }
+
+
+        adapter?.setOnClickDeleteItem {
+            deleteLocation(it.id)
+        }
+    }
+
+
+
+    private fun getLocations() {
+        val locList=sqLiteHelper.getAllLocations()
+        Log.e("pppp", "${locList.size}")
+
+        adapter?.addItems(locList)
+    }
+
+    private fun addLocation()
+    {
+        val loc = edlocation.text.toString()
+        val longitude = edlongitude.text.toString()
+        val latitude = edlatitude.text.toString()
+
+        if(loc.isEmpty()||longitude.isEmpty()||latitude.isEmpty())
+        {
+
+            Toast.makeText(this,"Please enter fields",Toast.LENGTH_SHORT).show()
+        }else
+        {
+
+            val loc=LocationModel(loc =loc,longitude = longitude,latitude = latitude)
+            val status=sqLiteHelper.insertLocation(loc)
+
+            if(status >-1)
+            {
+                Toast.makeText(this,"Location Added....",Toast.LENGTH_SHORT).show()
+                clearEditText()
+            }
+            else
+            {
+                Toast.makeText(this,"Not Added....",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun deleteLocation(id:Int)
+    {
+
+        val builder =AlertDialog.Builder(this)
+        builder.setMessage("Are you sure to DELETE ?")
+        builder.setCancelable(true)
+        builder.setPositiveButton("YES")
+        {dialog,_->
+            sqLiteHelper.deleteLocationById(id)
+            getLocations()
+
+            dialog.dismiss()
+
+        }
+        builder.setNegativeButton("No")
+        {
+                dialog,_->
+            dialog.dismiss()
+
+        }
+
+        val alert = builder.create()
+        alert.show()
+    }
+
+    private fun clearEditText()
+    {
+        edlocation.setText("")
+        edlongitude.setText("")
+        edlatitude.setText("")
+
+        edlocation.requestFocus()
+    }
+
+    private fun initRecycleView()
+    {
+
+        recyclerView.layoutManager= LinearLayoutManager(this)
+        adapter = LocationAdapter()
+        recyclerView.adapter = adapter
+    }
+    private fun initView()
+    {
+        edlocation=findViewById(R.id.edlocation)
+        edlongitude=findViewById(R.id.edlongitude)
+        edlatitude=findViewById(R.id.edlatitude)
+
+        btnAdd=findViewById(R.id.btnAdd)
+        btnView=findViewById(R.id.btnView)
+        recyclerView=findViewById(R.id.recyclerView)
+    }
+}
